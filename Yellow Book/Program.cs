78ad52c4057b9c563e579a63a2783594f8466322
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-03-14 21:01:00 -0300

    Do... while

--- Yellow Book/Program.cs
+++ Yellow Book/Program.cs
@@ -11,49 +11,27 @@ namespace Yellow_Book
         static void Main(string[] args)
         {
             double width, height, woodLength, glassArea;
-
             const double MAX_WIDTH = 5.0;
             const double MIN_WIDTH = 0.5;
             const double MAX_HEIGHT = 3.0;
             const double MIN_HEIGHT = 0.75;
-
             string widthString, heightString;
 
-            Console.WriteLine("Give the width of the window : ");
-            widthString = Console.ReadLine();
-            width = double.Parse(widthString);
-
-            if (width < MIN_WIDTH)
+            do
             {
-                Console.WriteLine("Width is too small.\n\n");
-                Console.WriteLine("Using minimum");
-                width = MIN_WIDTH;
-            }
+                Console.WriteLine("Give the width of the window between " +
+                    MIN_WIDTH + " and " + MAX_WIDTH);
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
+            } while (width < MIN_WIDTH || width > MAX_WIDTH);
 
-            if (width > MAX_WIDTH)
+            do
             {
-                Console.WriteLine("Width is too large.\n\n");
-                Console.WriteLine("Using maximum");
-                width = MAX_WIDTH;
-            }
-
-            Console.WriteLine("Give the height of the window: ");
-            heightString = Console.ReadLine();
-            height = double.Parse(heightString);
-
-            if (height < MIN_HEIGHT)
-            {
-                Console.WriteLine("Height is too small.\n\n");
-                Console.WriteLine("Using minimum");
-                height = MIN_HEIGHT;
-            }
-
-            if (height > MAX_HEIGHT)
-            {
-                Console.WriteLine("Height is too large.\n\n");
-                Console.WriteLine("Using maximum");
-                height = MAX_HEIGHT;
-            }
+                Console.WriteLine("Give the height of the window between " +
+                    MIN_HEIGHT + " and " + MAX_HEIGHT);
+                heightString = Console.ReadLine();
+                height = double.Parse(heightString);
+            } while (height < MIN_HEIGHT || height > MAX_HEIGHT);
 
             woodLength = 2 * (width + height) * 3.25;
 
